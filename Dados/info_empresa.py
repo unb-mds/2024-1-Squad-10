06adf331e3489f778b2e7516ff1b8bd922173aee
mat054 <mateus.castro3@gmail.com>
2024-06-22 21:54:45 -0300

    adição do arquivo que foi mudado de nome

--- Dados/info_empresa.py
+++ Dados/info_empresa.py
@@ -2,7 +2,7 @@ import json
 import pandas as pd
 
 # Carrega o arquivo JSON original
-with open('C:/Users/mateu/Documents/PROGRAMATION/Projeto MDS/Coleta_API/contratos_OFICIAL.json', 'r', encoding='utf-8') as file:
+with open('contratos_OFICIAL.json', 'r', encoding='utf-8') as file:
     dados = json.load(file)
 
 # Lista para armazenar os novos dicionários
