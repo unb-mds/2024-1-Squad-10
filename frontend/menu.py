cee81f0e266a4ce0042a23b44fdae2039e1f8b73
Rafael Matuda <rafael.matuda10@gmail.com>
2024-07-01 14:12:01 -0300

    atualizando a página home

--- frontend/menu.py
+++ frontend/menu.py
@@ -1,27 +1,41 @@
 import streamlit as st
 import streamlit.components.v1 as components
+import base64
 
 st.set_page_config(layout="wide")
+
 # Função para carregar o conteúdo do arquivo
 def load_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         return file.read()
 
-# Carregar o HTML, CSS e JavaScript
+# Função para carregar uma imagem e convertê-la para base64
+def load_image_base64(image_path):
+    try:
+        with open(image_path, 'rb') as img_file:
+            return base64.b64encode(img_file.read()).decode()
+    except FileNotFoundError:
+        return None
+
+# Carregar o HTML e CSS
 html_content = load_file('index.html')
 css_content = load_file('style.css')
-js_content = load_file('script.js')
 
-# Injetar o CSS e JavaScript no HTML
-html_with_css_js = f"""
+# Carregar e codificar as imagens
+logo_base64 = load_image_base64('images/licitanow.png')
+licitacao_base64 = load_image_base64('images/licitacao.png')
+
+# Atualizar o HTML com as imagens base64
+html_content = html_content.replace('images/licitanow.png', f'data:image/png;base64,{logo_base64}')
+html_content = html_content.replace('images/licitacao.png', f'data:image/png;base64,{licitacao_base64}')
+
+# Injetar o CSS no HTML
+html_with_css = f"""
     <style>
     {css_content}
     </style>
     {html_content}
-    <script>
-    {js_content}
-    </script>
 """
 
-# Exibir o HTML com CSS e JavaScript no Streamlit
-components.html(html_with_css_js, height=800, scrolling=True)
+# Exibir o HTML com CSS no Streamlit
+components.html(html_with_css, height=800, scrolling=False)
