2d7107ae7975f6a5cb5d4d4b1b332be265e9116a
davi-aguiar-vieira <davideaguiarvieira@gmail.com>
2024-07-01 21:39:51 -0300

    Arrumando o endereço das imagens e formatando as fotos

--- Dados/sobrenos.py
+++ Dados/sobrenos.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from PIL import Image, ImageOps
+import os
 
 def apply_dark_mode_css():
     st.markdown("""
@@ -20,6 +22,11 @@ def apply_dark_mode_css():
         </style>
         """, unsafe_allow_html=True)
 
+def resize_image(image_path, size=(150, 150)):
+    img = Image.open(image_path)
+    img = ImageOps.fit(img, size, Image.ANTIALIAS)
+    return img
+
 apply_dark_mode_css()
 
 st.header('Sobre nós')
@@ -30,27 +37,33 @@ st.write("""
 """)
 st.header('Participantes')
 
+# Defina o caminho absoluto para a pasta de imagens
+image_folder = os.path.join(os.path.dirname(__file__), '../docs/imagens')
+
+def get_image_path(image_name):
+    return os.path.join(image_folder, image_name)
+
 col1, col2, col3, col4 = st.columns(4)
 
 with col1:
-    st.image(r'C:\Users\olavo\OneDrive\Pictures\Screenshots\Mateus.png', caption='Mateus de Castro', width=150)
+    st.image(resize_image(get_image_path('mateus.png')), caption='Mateus de Castro')
 
 with col2:
-    st.image(r'C:\Users\olavo\OneDrive\Pictures\Screenshots\Rômulo.png', caption='Rômulo de Araújo', width=150)
+    st.image(resize_image(get_image_path('romulo.png')), caption='Rômulo de Araújo')
 
 with col3:
-    st.image(r'C:\Users\olavo\OneDrive\Pictures\Screenshots\Davi.png', caption='Davi de Aguiar', width=150)
+    st.image(resize_image(get_image_path('lindo.png')), caption='Davi de Aguiar')
 
 with col4:
-    st.image(r'C:\Users\olavo\Downloads\henrique.jpeg', caption='Henrique Carvalho', width=150)
+    st.image(resize_image(get_image_path('henrique.jpeg')), caption='Henrique Carvalho')
 
 col5, col6, col7 = st.columns(3)
 
 with col5:
-    st.image(r'C:\Users\olavo\OneDrive\Pictures\Screenshots\Rafael.png', caption='Rafael Melo', width=150)
+    st.image(resize_image(get_image_path('rafa.png')), caption='Rafael Melo')
 
 with col6:
-    st.image(r'C:\Users\olavo\Downloads\Lock.jpeg', caption='Pedro Lock', width=150)
+    st.image(resize_image(get_image_path('Pedrolock.jpeg')), caption='Pedro Lock')
 
 with col7:
-    st.image(r'C:\Users\olavo\OneDrive\Pictures\Screenshots\clara.png', caption='Maria Clara', width=150)
+    st.image(resize_image(get_image_path('clara.jpeg')), caption='Maria Clara')
