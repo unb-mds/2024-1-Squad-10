d42bc2da07df1006480cadb7c388e2159748be86
mat054 <mateus.castro3@gmail.com>
2024-07-29 13:49:40 -0300

    Nova função dentro do arquivo logging_config.py (funcionalidade para log dos arquivos que deram erro na segunda coleta de dados

--- Dados/logging_config.py
+++ Dados/logging_config.py
@@ -2,12 +2,30 @@ import logging
 
 def setup_logger():
     logging.basicConfig(level=logging.INFO,
-                        filename='info01.log',
+                        filename='info.log',
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     )
 
     # Configuração de um logger específico (opcional)
     logger = logging.getLogger('logger_coletaAPI')
+    logger.setLevel(logging.DEBUG)
+    
+    # Handler para console (opcional)
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    console_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    logger.addHandler(console_handler)
+
+    return logger
+
+    def setup_logger_01():
+    logging.basicConfig(level=logging.INFO,
+                        filename='info_erros.log',
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+
+    # Configuração de um logger específico (opcional)
+    logger = logging.getLogger('logger_coleta_erros')
     logger.setLevel(logging.DEBUG)
     
     # Handler para console (opcional)
