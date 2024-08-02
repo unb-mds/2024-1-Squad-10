05fee9d25d32a01700523fed45591f8a6553059d
mat054 <mateus.castro3@gmail.com>
2024-08-02 00:28:22 -0300

    Refatoração total do código de coleta das informações dos CNPJs e ajuste de erro no código Coleta_api

--- Dados/Coleta_dados/coleta_cnpjs.py
+++ Dados/Coleta_dados/coleta_cnpjs.py
@@ -1,54 +1,86 @@
+# ARQUIVO REFATORADO
+
 import json
+import csv
 import requests
+import time
+import sys
 
-# Caminho do arquivo JSON
-with open('infos_cnpj_OFICIAL.json', 'w', encoding='utf-8') as f:
-    f.write('[\n')
-    # Abre o arquivo JSON para leitura
-    with open('contratos_OFICIAL.json', 'r', encoding='utf-8') as arquivo:
-        # Carrega o conteúdo do arquivo como um objeto Python
-        dados_json = json.load(arquivo)
+# Cria a lista dos cnpjs, tira os repetidos e coloca em ordem crescente
+def lista_cnpjs (arquivo):
+    with open(arquivo,'r',encoding='utf-8') as file:
+        dados_json = csv.reader(file)
+        lista = [linha[-3] for linha in dados_json if (linha[-3].isnumeric() and len(linha[-3])==14)] # faz uma lista com o elemento -3, que corresponde ao cnpj
+        lista_oficial = list(set(lista))
+        lista_oficial.sort()
+    return lista_oficial
 
-        # Conjunto para armazenar CNPJs já processados
-        cnpjs_processados = set()
+# Pega a lista e divide em 3 listas de tamanhos iguais
+def lista_3_listas(lista):
+    tamanho = len(lista) // 3
+    return [lista[i * tamanho: (i + 1) * tamanho] for i in range(3)]
 
-        # Itera sobre cada item do arquivo JSON
-        for item in dados_json:
-            for chave, valor in item['Empresas Contratadas'].items():
-                # Verifica se a chave começa com "CNPJ"
-                if chave.startswith('CNPJ'):
-                    if valor not in cnpjs_processados:
-                        try:
-                            # Tenta obter os dados do CNPJ
-                            url = 'https://api.cnpjs.dev/v1/'+str(valor)
-                            response = requests.get(url)
 
-                            if response.status_code == 200:
-                                data = response.json()
-                                # Verifica se o CNPJ já foi processado antes                            
-                                
-                                informations = {
-                                    "CNPJ" : data["cnpj"],
-                                    "Razão social" : data["razao_social"],
-                                    "Porte" : data["porte"],
-                                    "Nome Fantasia" : data["nome_fantasia"],
-                                    "Situação Cadastral" : data["situacao_cadastral"],
-                                    "Data da Situação Cadastral" : data["data_situacao_cadastral"],
-                                    "CNAE fiscal principal": data["cnae_fiscal_principal"],
-                                    "Endereço UF" : data["endereco"]["uf"],
-                                    "Endereço Município" : data["endereco"]["municipio"],
-                                    "Data de Início da Atividade" : data["data_inicio_atividade"],
-                                    "Sócios" : data["socios"]
-                                }
-                                nCNPJ = data["cnpj"]
-                                print(nCNPJ)
-                                json.dump(informations, f, ensure_ascii=False, indent=4)
-                                f.write(',\n')
-                                # Adiciona o CNPJ ao conjunto de CNPJs processados
-                                cnpjs_processados.add(valor)
-                        except KeyError:
-                            # Se a chave 'cnpj' não for encontrada, continue para o próximo loop
-                            continue
-        f.seek(f.tell() - 3)  # Move o cursor de escrita de volta 3 caracteres
-        f.truncate()  # Remove o último caractere (a vírgula)
-        f.write('\n]')  # Escreve o fechamento da lista
+# retorna a lista das empresas contratadas
+CNPJs = lista_cnpjs("../../frontend/x_empresas_contratadas.csv") # inicializa a primeira função
+listas_cnpjs = lista_3_listas(CNPJs) # inicializa a segunda função
+
+wait_time = 1
+posicao = int(sys.argv[1])
+
+print(len(listas_cnpjs[posicao])) # printa o tamanho da lista que iremos percorrer
+
+with open('../../frontend/infos_cnpj_OFICIAL.json', sys.argv[2], encoding='utf-8') as f:
+    f.write(sys.argv[3])
+    for index,cnpj in enumerate(listas_cnpjs[posicao]):
+        wait_time =1
+        time.sleep(5)
+
+        for tentativa in range(5):
+            try:
+                # Tenta obter os dados do CNPJ
+                url = f'https://api.cnpjs.dev/v1/{cnpj}'
+                response = requests.get(url)
+                response.raise_for_status()
+                
+                
+                data = response.json()
+                # Verifica se o CNPJ já foi processado antes                            
+                
+                informations = {
+                    "CNPJ" : data["cnpj"],
+                    "Razão social" : data["razao_social"],
+                    "Porte" : data["porte"],
+                    "Nome Fantasia" : data["nome_fantasia"],
+                    "Situação Cadastral" : data["situacao_cadastral"],
+                    "Data da Situação Cadastral" : data["data_situacao_cadastral"],
+                    "CNAE fiscal principal": data["cnae_fiscal_principal"],
+                    "Endereço UF" : data["endereco"]["uf"],
+                    "Endereço Município" : data["endereco"]["municipio"],
+                    "Data de Início da Atividade" : data["data_inicio_atividade"],
+                    "Sócios" : data["socios"]
+                }
+                nCNPJ = data["cnpj"]
+                print(f"{nCNPJ} coletado com sucesso. Posição na fila: {index+1}")
+                json.dump(informations, f, ensure_ascii=False, indent=4)
+                if (cnpj == lista_cnpjs[posicao][-1]):
+                    f.write(sys.argv[4])
+                else:
+                    f.write(',\n')
+                break
+
+            except:
+                if (tentativa < 4):
+                    time.sleep(wait_time) # Espera o wait time para fazer a próxima requisição
+                    wait_time *= 2 # Dobra o tempo de espera para a próxima tentativa de requisição (caso exista)
+                    print(f"Tentativa numero {tentativa} do CNPJ {cnpj}")
+                else:
+                    print(f"erro ao coletar dados do cnpj {cnpj}")
+
+
+# Entrada no terminal: 
+# python coleta_cnpj.py "0" "w" "\n[" ","
+# python coleta_cnpj.py "1" "a" "\n" ","
+# python coleta_cnpj.py "2" "a" "\n" "\n]"
+
+
