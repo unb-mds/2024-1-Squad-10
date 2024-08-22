ff0e520bb176ca1b1bfeaadee9de279d7d7306d8
henriquecarv3 <134213039+henriquecarv3@users.noreply.github.com>
2024-08-22 19:03:46 -0300

    Refatoração do código info_empresa

--- Dados/info_empresa.py
+++ Dados/info_empresa.py
@@ -1,6 +1,10 @@
+"""
+Script para processar dados de contratos de um arquivo JSON e salvar em um arquivo CSV.
+"""
+
 import json
-import pandas as pd
 import os
+import pandas as pd
 
 def carregar_dados_json(caminho_arquivo_json):
     """Carrega e retorna os dados de um arquivo JSON."""
@@ -11,15 +15,15 @@ def carregar_dados_json(caminho_arquivo_json):
 def processar_contratos(dados):
     """Processa os dados dos contratos para extrair a estrutura desejada."""
     nova_estrutura = []
-    
+
     for contrato in dados:
         codigo = contrato["Código"]
         empresas_contratadas = contrato["Empresas Contratadas"]
-        
+
         for chave in empresas_contratadas:
             if chave.startswith("Empresa Contratada"):
                 indice = chave.split(" -")[-1]
-                
+
                 novo_dict = {
                     "Código": codigo,
                     "Empresa Contratada": empresas_contratadas[f"Empresa Contratada -{indice}"],
@@ -27,9 +31,9 @@ def processar_contratos(dados):
                     "Valor Recebido": empresas_contratadas[f"Valor Recebido -{indice}"],
                     "Descrição": empresas_contratadas[f"Descrição -{indice}"]
                 }
-                
+
                 nova_estrutura.append(novo_dict)
-    
+
     return nova_estrutura
 
 def salvar_em_csv(dados, caminho_arquivo_csv):
@@ -39,18 +43,19 @@ def salvar_em_csv(dados, caminho_arquivo_csv):
     return os.path.abspath(caminho_arquivo_csv)
 
 def main():
+    """Função principal que coordena o carregamento, processamento e salvamento dos dados."""
     caminho_arquivo_json = 'frontend/contratos_OFICIAL.json'
     caminho_arquivo_csv = 'frontend/x_empresas_contratadas.csv'
-    
+
     # Carrega os dados do arquivo JSON
     dados = carregar_dados_json(caminho_arquivo_json)
-    
+
     # Processa os dados dos contratos
     nova_estrutura = processar_contratos(dados)
-    
+
     # Salva os dados processados em um arquivo CSV
     caminho_absoluto_csv = salvar_em_csv(nova_estrutura, caminho_arquivo_csv)
-    
+
     print(f"O arquivo foi salvo em: {caminho_absoluto_csv}")
     print("Arquivo CSV criado com sucesso!")
 
